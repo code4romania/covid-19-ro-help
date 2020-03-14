94c06bb679be91685b41c61b30ad2229f2b66bf6
catileptic <s.alexandra.stefanescu@gmail.com>
2020-03-14 17:11:25 +0200

    Add navbar (#16)
    * Navbar initial commit
    
    * Add logos under navbar
    
    * Remove square

--- aid_management/hub/urls.py
+++ aid_management/hub/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
