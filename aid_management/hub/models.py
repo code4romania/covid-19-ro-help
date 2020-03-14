378de68122fcbe325042e1fd3820518052f446c7
Vlad Temian <vladtemian@gmail.com>
2020-03-14 15:08:27 +0200

    Seed some more random data

--- aid_management/hub/models.py
+++ aid_management/hub/models.py
@@ -2,10 +2,34 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
+class URGENCY:
+    CRITICAL = "critical"
+    HIGH = "high"
+    MEDIUM = "medium"
+    LOW = "low"
+
+    @classmethod
+    def to_choices(cls):
+        return [
+            (URGENCY.CRITICAL, URGENCY.CRITICAL),
+            (URGENCY.HIGH, URGENCY.HIGH),
+            (URGENCY.MEDIUM, URGENCY.MEDIUM),
+            (URGENCY.LOW, URGENCY.LOW),
+        ]
+
+    @classmethod
+    def default(cls):
+        return URGENCY.LOW
+
+    @classmethod
+    def to_list(cls):
+        return [URGENCY.CRITICAL, URGENCY.HIGH, URGENCY.MEDIUM, URGENCY.LOW]
+
+
 class KIND:
     RESOURCE = "resource"
     VOLUNTEER = "volunteer"
-    MONEY = "Money"
+    MONEY = "money"
 
     @classmethod
     def to_choices(cls):
@@ -26,16 +50,44 @@ class KIND:
 
 class NGO(models.Model):
     name = models.CharField(_("Name"), max_length=254,)
-    description = models.CharField(_("Description"), max_length=1024)
+    description = models.CharField(_("Description"), max_length=2048)
     email = models.EmailField(_("Email"), )
-    phone = models.CharField(_("Phone"), max_length=15)
-    avatar = models.ImageField(_("Avatar"), max_length=15)
+    phone = models.CharField(_("Phone"), max_length=30)
+    avatar = models.ImageField(_("Avatar"), max_length=300)
     address = models.CharField(_("Address"), max_length=400)
     city = models.CharField(_("City"), max_length=100)
     county = models.CharField(_("County"), max_length=50)
 
+    def __str__(self):
+        return self.name
 
-# class NGONeed(models.Model):
-#     ngo = models.ForeignKey(NGO, on_delete=models.CASCADE)
-#     description = models.CharField(_("Description"), )
-#     kind = models.Choices(_())
+
+class NGONeed(models.Model):
+    ngo = models.ForeignKey(NGO, on_delete=models.CASCADE)
+
+    description = models.CharField(_("Description"), max_length=4096)
+
+    kind = models.CharField(_("Kind"), choices=KIND.to_choices(), default=KIND.default(), max_length=10)
+    urgency = models.CharField(_("Urgency"), choices=URGENCY.to_choices(), default=URGENCY.default(), max_length=10)
+
+    def __str__(self):
+        return f"{self.ngo.name}: {self.urgency} {self.kind}"
+
+
+class PersonalRequest(models.Model):
+    ngo = models.ForeignKey(NGO, on_delete=models.CASCADE, null=True, blank=True)
+
+    name = models.CharField(_("Name"), max_length=254,)
+    email = models.EmailField(_("Email"), null=True, blank=True)
+    phone = models.CharField(_("Phone"), max_length=15)
+    city = models.CharField(_("City"), max_length=100)
+    county = models.CharField(_("County"), max_length=50)
+    address = models.CharField(_("Address"), max_length=400, null=True, blank=True)
+    organization = models.CharField(_("Organization"), max_length=400, null=True, blank=True)
+    description = models.CharField(_("Description"), max_length=1024)
+
+    kind = models.CharField(_("Kind"), choices=KIND.to_choices(), default=KIND.default(), max_length=10)
+    urgency = models.CharField(_("Urgency"), choices=URGENCY.to_choices(), default=URGENCY.default(), max_length=10)
+
+    def __str__(self):
+        return self.name
