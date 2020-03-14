2909673db433311a5843e5ae2d7e59ed01850bf3
Vlad Temian <vladtemian@gmail.com>
2020-03-14 12:40:12 +0200

    Initial docker-compose and models setup (#15)

---
+++ Makefile
@@ -0,0 +1,2 @@
+lint:
+	 black --exclude venv/ --line-length 120 --target-version py37 .
--- aid_management/aid_management/wsgi.py
+++ aid_management/aid_management/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aid_management.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aid_management.settings")
 
 application = get_wsgi_application()
