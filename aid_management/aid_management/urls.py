901b2996343cfb8c4190d42f88a5a4d104dcc450
Vlad Temian <vladtemian@gmail.com>
2020-03-14 15:08:27 +0200

    Seed some more random data

--- aid_management/aid_management/urls.py
+++ aid_management/aid_management/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +23,4 @@ urlpatterns = [
     path("", include('hub.urls')),
 
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- aid_management/hub/admin.py
+++ aid_management/hub/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import NGO, NGONeed, PersonalRequest
 
+
+@admin.register(NGO)
+class NGOAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "city", "county", )
+    list_filter = ("city", "county",)
+    search_fields = ("name", "email", )
+
+
+@admin.register(NGONeed)
+class NGONeedAdmin(admin.ModelAdmin):
+    list_display = ("ngo", "urgency", "kind",)
+    list_filter = ("ngo", "urgency", "kind", "ngo__city", "ngo__county",)
+    search_fields = ("ngo__name", "ngo__email", )
+
+
+@admin.register(PersonalRequest)
+class PersonalRequestAdmin(admin.ModelAdmin):
+    pass
