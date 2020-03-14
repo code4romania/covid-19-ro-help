deb61f7a93e17a5c5c80be1cdcb5ca76dff2f8fe
Vlad Temian <vladtemian@gmail.com>
2020-03-14 12:40:12 +0200

    Initial docker-compose and models setup (#15)

---
+++ aid_management/aid_management/settings/dev.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = TEMPLATE_DEBUG = True
+
+INTERNAL_IPS = ["127.0.0.1", "localhost"]
+
+AUTH_PASSWORD_VALIDATORS = []
+
+# Add debug toolbar
+if DEBUG:
+    INSTALLED_APPS += ["debug_toolbar", "django_extensions"]
+    MIDDLEWARE.insert(1, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+# TODO: read it from env or generate a new one
+SECRET_KEY = "https://uploads.skyhighnetworks.com/wp-content/uploads/2015/08/06195203/Bart-Chalkboard-for-Blog-Post.png"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
---
+++ aid_management/hub/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class KIND:
+    RESOURCE = "resource"
+    VOLUNTEER = "volunteer"
+    MONEY = "Money"
+
+    @classmethod
+    def to_choices(cls):
+        return [
+            (KIND.RESOURCE, KIND.RESOURCE),
+            (KIND.VOLUNTEER, KIND.VOLUNTEER),
+            (KIND.MONEY, KIND.MONEY),
+        ]
+
+    @classmethod
+    def default(cls):
+        return KIND.RESOURCE
+
+    @classmethod
+    def to_list(cls):
+        return [KIND.RESOURCE, KIND.VOLUNTEER, KIND.MONEY]
+
+
+class NGO(models.Model):
+    name = models.CharField(_("Name"), max_length=254,)
+    description = models.CharField(_("Description"), )
+    email = models.EmailField(_("Email"), )
+    phone = models.CharField(_("Phone"), max_length=15)
+    avatar = models.ImageField(_("Avatar"), max_length=15)
+    address = models.CharField(_("Address"), max_length=400)
+    city = models.CharField(_("City"), max_length=100)
+    county = models.CharField(_("County"), max_length=50)
+
+
+# class NGONeed(models.Model):
+#     ngo = models.ForeignKey(NGO, on_delete=models.CASCADE)
+#     description = models.CharField(_("Description"), )
+#     kind = models.Choices(_())
