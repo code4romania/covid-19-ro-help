0911fd15563b054e566edf1245b07f2e28b02a2a
Vlad Temian <vladtemian@gmail.com>
2020-03-17 21:14:05 +0200

    Paint it black

--- ro_help/ro_help/urls.py
+++ ro_help/ro_help/urls.py
@@ -19,9 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
-    path("", include("hub.urls")),
-    path("admin/", admin.site.urls),
-) + static(
+urlpatterns = i18n_patterns(path("", include("hub.urls")), path("admin/", admin.site.urls),) + static(
     settings.STATIC_URL, document_root=settings.STATIC_ROOT
 )
