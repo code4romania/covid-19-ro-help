37a18912e422ed999ca9c31f064d83e02c2ade4f
Costin Bleotu <costi.bleotu@gmail.com>
2020-03-17 17:57:19 +0200

    added language  in url

--- ro_help/ro_help/urls.py
+++ ro_help/ro_help/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("", include("hub.urls")),
     path("admin/", admin.site.urls),
-] + static(
+) + static(
     settings.STATIC_URL, document_root=settings.STATIC_ROOT
 )
