8a24a214c39ce710eb56062ec7ce4d35a33b200f
Vlad Temian <vladtemian@gmail.com>
2020-03-19 20:46:38 +0200

    Remove unused imports

--- ro_help/hub/forms.py
+++ ro_help/hub/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
-from django_crispy_bulma.forms import EmailField
+
 from django_crispy_bulma.widgets import EmailInput
 
 from hub import models
@@ -20,8 +19,8 @@ class NGORegisterRequestForm(forms.ModelForm):
 
     class Meta:
         model = models.RegisterNGORequest
-        fields = fields = ["name", "county", "city", "address", "avatar", "email", "contact_name",
-              "contact_phone", "email", "social_link", "description", "has_netopia_contract"]
+        fields = ["name", "county", "city", "address", "avatar", "email", "contact_name",
+                  "contact_phone", "email", "social_link", "description", "has_netopia_contract"]
         widgets = {
             'email': EmailInput()
         }
