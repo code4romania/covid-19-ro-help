6f4ded68dd31c4454d1ba39055700d5d8b3bd7dc
Iulian Stana <julian.stana@gmail.com>
2020-03-28 16:53:29 +0200

    Add validation for avatar on NGORegisterRequestForm  #113

--- ro_help/hub/forms.py
+++ ro_help/hub/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django_crispy_bulma.widgets import EmailInput, FileUploadInput
+from django.core.files.images import get_image_dimensions
 from file_resubmit.admin import AdminResubmitFileWidget, AdminResubmitImageWidget
 
 from hub import models
@@ -45,6 +46,34 @@ class NGORegisterRequestForm(forms.ModelForm):
             "last_balance_sheet": AdminResubmitFileWidget,
             "statute": AdminResubmitFileWidget,
         }
+
+    def clean_avatar(self):
+        avatar = self.cleaned_data['avatar']
+
+        try:
+            w, h = get_image_dimensions(avatar)
+
+            #validate dimensions
+            max_width = max_height = 500
+            if w > max_width or h > max_height:
+                raise forms.ValidationError(u'Please use an image that is '
+                                            '%s x %s pixels or smaller.' %
+                                            (max_width, max_height))
+
+            #validate content type
+            main, sub = avatar.content_type.split('/')
+            if not (main == 'image'
+                    and sub in ['jpeg', 'pjpeg', 'png']):
+                raise forms.ValidationError(u'Please use a JPEG or PNG image.')
+        
+        except AttributeError:
+            """
+            Handles case when we are updating the user profile
+            and do not supply a new avatar
+            """
+            pass
+        
+        return avatar
 
 
 class RegisterNGORequestVoteForm(forms.ModelForm):
