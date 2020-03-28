78ba7546cedcc4f951040d421c4da52fc2aee788
Alexandru Gherghel <alecs@alecs.eu>
2020-03-28 17:41:30 +0200

    added custom password validator.

--- ro_help/ro_help/settings/base.py
+++ ro_help/ro_help/settings/base.py
@@ -123,6 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "hub.password_validation.PasswordDifferentFromPrevious"}
 ]
 
 
