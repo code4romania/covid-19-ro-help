f36f79c04a996d57f9966c3289e5e5fdd63a69a3
Alexandru Gherghel <alecs@alecs.eu>
2020-03-28 15:10:57 +0200

    moved build_full_url to hub/utils.py.

--- ro_help/hub/templatetags/social_share.py
+++ ro_help/hub/templatetags/social_share.py
@@ -1,5 +1,7 @@
 from django import template
 
+from hub.utils import build_full_url
+
 TWITTER_ENDPOINT = "https://twitter.com/intent/tweet?text=%s"
 FACEBOOK_ENDPOINT = "https://www.facebook.com/sharer/sharer.php?u=%s"
 LINKEDIN_ENDPOINT = "https://www.linkedin.com/sharing/share-offsite/?url=%s"
@@ -7,14 +9,10 @@ LINKEDIN_ENDPOINT = "https://www.linkedin.com/sharing/share-offsite/?url=%s"
 register = template.Library()
 
 
-def _build_full_url(request, obj):
-    return request.build_absolute_uri(obj.get_absolute_url())
-
-
 @register.inclusion_tag("partials/social_sharing.html", takes_context=True)
 def social_buttons(context, obj):
     request = context["request"]
-    full_path_url = _build_full_url(request, obj)
+    full_path_url = build_full_url(request, obj)
 
     context["facebook_url"] = FACEBOOK_ENDPOINT % full_path_url
     context["twitter_url"] = TWITTER_ENDPOINT % full_path_url
--- ro_help/hub/utils.py
+++ ro_help/hub/utils.py
@@ -11,3 +11,13 @@ def send_email(template, context, subject, to):
     msg.attach_alternative(html_content, "text/html")
 
     return msg.send()
+
+
+def build_full_url(request, obj):
+    """
+    :param request: django Request object
+    :param obj: any obj that implements get_absolute_url() and for which
+    we can generate a unique URL
+    :return: returns the full URL towards the obj detail page (if any)
+    """
+    return request.build_absolute_uri(obj.get_absolute_url())
