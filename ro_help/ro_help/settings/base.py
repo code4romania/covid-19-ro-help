f9067ec267da08a8b079df2fe41146b3aecd0442
catileptic <s.alexandra.stefanescu@gmail.com>
2020-03-28 13:54:00 +0200

    Add media file config to settings (#129)

--- ro_help/ro_help/settings/base.py
+++ ro_help/ro_help/settings/base.py
@@ -165,6 +165,7 @@ if USE_S3:
     AWS_LOCATION = "static"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     STATIC_URL = "/static/"
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
