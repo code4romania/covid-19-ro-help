26dd6f99a953a64af3a167b1e3a3b78edf559cf1
Andrei Mirestean <andrei.mirestean@gmail.com>
2020-04-02 21:20:59 +0300

    Store private media on disk when not running on AWS

--- ro_help/hub/models.py
+++ ro_help/hub/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.models import User, Group
+from django.core.files.storage import get_storage_class
 from django.db import models, transaction
 from django.utils import timezone
 from django.utils.crypto import get_random_string
@@ -7,12 +9,13 @@ from django.utils.translation import ugettext_lazy as _
 
 from django_extensions.db.models import TimeStampedModel
 
-from .storage_backends import PublicMediaStorage, PrivateMediaStorage
-
 ADMIN_GROUP_NAME = "Admin"
 NGO_GROUP_NAME = "ONG"
 DSU_GROUP_NAME = "DSU"
 FFC_GROUP_NAME = "FFC"
+
+PrivateMediaStorageClass = get_storage_class(settings.PRIVATE_FILE_STORAGE)
+PublicMediaStorageClass = get_storage_class(settings.DEFAULT_FILE_STORAGE)
 
 
 class URGENCY:
@@ -200,11 +203,13 @@ class NGO(TimeStampedModel):
     county = models.CharField(_("County"), choices=COUNTY.to_choices(), max_length=50)
     city = models.CharField(_("City"), max_length=100)
 
-    avatar = models.ImageField(_("Avatar"), max_length=300, storage=PublicMediaStorage())
+    avatar = models.ImageField(_("Avatar"), max_length=300, storage=PublicMediaStorageClass())
     last_balance_sheet = models.FileField(
-        _("First page of last balance sheet"), max_length=300, null=True, blank=True, storage=PrivateMediaStorage()
+        _("First page of last balance sheet"), max_length=300, null=True, blank=True, storage=PrivateMediaStorageClass()
     )
-    statute = models.FileField(_("NGO Statute"), max_length=300, null=True, blank=True, storage=PrivateMediaStorage())
+    statute = models.FileField(
+        _("NGO Statute"), max_length=300, null=True, blank=True, storage=PrivateMediaStorageClass()
+    )
 
     accepts_mobilpay = models.BooleanField(_("Accepts mobilpay"), default=False)
     accepts_transfer = models.BooleanField(_("Accepts transfers"), default=False)
@@ -218,10 +223,10 @@ class NGO(TimeStampedModel):
         help_text=_("XXXX-XXXX-XXXX-XXXX-XXXX"),
     )
     mobilpay_public_key = models.FileField(
-        _("mobilpay Public key"), max_length=300, null=True, blank=True, storage=PrivateMediaStorage()
+        _("mobilpay Public key"), max_length=300, null=True, blank=True, storage=PrivateMediaStorageClass()
     )
     mobilpay_private_key = models.FileField(
-        _("mobilpay Private key"), max_length=300, null=True, blank=True, storage=PrivateMediaStorage()
+        _("mobilpay Private key"), max_length=300, null=True, blank=True, storage=PrivateMediaStorageClass()
     )
 
     def __str__(self):
@@ -384,8 +389,10 @@ class RegisterNGORequest(TimeStampedModel):
     resolved_on = models.DateTimeField(_("Resolved on"), null=True, blank=True)
 
     avatar = models.ImageField(_("Avatar"), max_length=300, help_text=_("Image should be 500x500px"))
-    last_balance_sheet = models.FileField(_("First page of last balance sheet"), max_length=300, storage=PrivateMediaStorage())
-    statute = models.FileField(_("NGO Statute"), max_length=300, storage=PrivateMediaStorage())
+    last_balance_sheet = models.FileField(
+        _("First page of last balance sheet"), max_length=300, storage=PrivateMediaStorageClass()
+    )
+    statute = models.FileField(_("NGO Statute"), max_length=300, storage=PrivateMediaStorageClass())
 
     registered_on = models.DateTimeField(_("Registered on"), auto_now_add=True)
 
