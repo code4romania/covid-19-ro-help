8efa4a8b3534e5826fae406e4d68a7441d30bb57
Alexandru Muntean <5768982+alex-muntean@users.noreply.github.com>
2020-04-06 18:11:41 +0300

    Added title block; Added og meta tags to base.html with defaults - sh… (#137)
    * Added title block; Added og meta tags to base.html with defaults - should be overwritten by each page as needed
    
    * Move meta tags to custom tag
    
    * Indentation fix
    
    * Improvements + code style fixes

---
+++ ro_help/hub/templatetags/meta_tags.py
@@ -0,0 +1,20 @@
+import os
+import urllib.parse
+
+from django import template
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+register = template.Library()
+
+@register.inclusion_tag('meta.html', takes_context=True)
+def meta_tags(context):
+    request = context['request']
+    image_path = urllib.parse.urljoin(settings.STATIC_URL, context.get('image', "images/logo-meta.png"))
+    return {
+        "url": request.build_absolute_uri,
+        "type": "website",
+        "title": _(context.get('title', "RO Help")),
+        "image": request.build_absolute_uri(image_path),
+        "description": _(context.get('description', "A coherent and safe collection of aid.")),
+    }
