19070ea662505449d09b93149c418b864397741c
Vlad Temian <vladtemian@gmail.com>
2020-05-18 21:24:51 +0300

    Allow debug and sentry to be configured from env

--- ro_help/ro_help/settings/prod.py
+++ ro_help/ro_help/settings/prod.py
@@ -1,8 +1,6 @@
 from .base import *
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
+# Fail-safe: make sure that we never run DEBUG in production
 DEBUG = TEMPLATE_DEBUG = False
 
 SECRET_KEY = env("SECRET_KEY")
@@ -13,11 +11,3 @@ MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "../", "static")
 STATICFILES_DIRS = []
-
-sentry_sdk.init(
-    dsn="https://fafd029cf71346c1b9c44397e6634b47@o375441.ingest.sentry.io/5194891",
-    integrations=[DjangoIntegration()],
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
