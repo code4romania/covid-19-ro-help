96bf708e10ed9ee0d009a2c3e09a3007c0e2fcaa
Alex Badea <vamposdecampos@gmail.com>
2021-04-24 12:50:00 -0700

    crypto-test: exercise encrypt-decrypt round trip
    This just excercises the code, but doesn't test correctness.

--- ro_help/mobilpay/mobilpay/test.py
+++ ro_help/mobilpay/mobilpay/test.py
@@ -12,6 +12,13 @@ class TestCrypto(unittest.TestCase):
         privkey = Crypto().get_private_key('pki/us-rsa1024/private.pem')
         self.assertIsNotNone(privkey)
         self.assertTrue(privkey)
+    def test_self_roundtrip(self):
+        pubkey = Crypto().get_rsa_key('pki/us-rsa1024/public.pem')
+        privkey = Crypto().get_private_key('pki/us-rsa1024/private.pem')
+        clear_msg = b'forty-two'
+        enc_msg, enc_block_key = Crypto().encrypt(clear_msg, pubkey)
+        dec_msg = Crypto().decrypt(enc_msg, privkey, enc_block_key)
+        self.assertEqual(dec_msg, clear_msg)
 
 if __name__ == "__main__":
     unittest.main()
