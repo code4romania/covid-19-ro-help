6c692ba8eef87e9fdd28132f47890acdd2d3bede
Alex Badea <vamposdecampos@gmail.com>
2021-04-24 12:14:51 -0700

    crypto-test: move import check into a unittest module
    No tests yet, but still verify import succeeds.

---
+++ ro_help/mobilpay/mobilpay/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import unittest
+from mobilpay.mobilpay.util.encrypt_data import Crypto
+
+class TestCrypto(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
