6ef4a7233fde20a35993dda5bd99d2e00514e986
Alex Badea <vamposdecampos@gmail.com>
2021-04-24 15:57:36 -0700

    crypto test: add decryption test case
    Using input data generated by openssl.

--- ro_help/mobilpay/mobilpay/test.py
+++ ro_help/mobilpay/mobilpay/test.py
@@ -19,6 +19,17 @@ class TestCrypto(unittest.TestCase):
         enc_msg, enc_block_key = Crypto().encrypt(clear_msg, pubkey)
         dec_msg = Crypto().decrypt(enc_msg, privkey, enc_block_key)
         self.assertEqual(dec_msg, clear_msg)
+    def test_decrypt(self):
+        d = 'pki/them-rsa1024/'
+        def _slurp(fname):
+            with open(d + fname, 'r') as fp:
+                return fp.read()
+
+        msg_enc = _slurp('msg.enc.b64')
+        enc_key_rsaenc = _slurp('enc.key.rsaenc.b64')
+        privkey = Crypto().get_private_key(d + 'private.pem')
+        dec_msg = Crypto().decrypt(msg_enc, privkey, enc_key_rsaenc)
+        self.assertEqual(dec_msg, b'message from them\n')
 
 if __name__ == "__main__":
     unittest.main()
