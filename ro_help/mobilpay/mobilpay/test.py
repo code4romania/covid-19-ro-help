c28f089180751828e6d62dcc656f92b0eb99bb72
Alex Badea <vamposdecampos@gmail.com>
2021-04-24 12:18:06 -0700

    crypto-test: add test cases for reading public/private key files
    Just verify they don't except and they return _something_.

--- ro_help/mobilpay/mobilpay/test.py
+++ ro_help/mobilpay/mobilpay/test.py
@@ -4,7 +4,14 @@ import unittest
 from mobilpay.mobilpay.util.encrypt_data import Crypto
 
 class TestCrypto(unittest.TestCase):
-    pass
+    def test_load_pub(self):
+        pubkey = Crypto().get_rsa_key('pki/us-rsa1024/public.pem')
+        self.assertIsNotNone(pubkey)
+        self.assertTrue(pubkey)
+    def test_load_priv(self):
+        privkey = Crypto().get_private_key('pki/us-rsa1024/private.pem')
+        self.assertIsNotNone(privkey)
+        self.assertTrue(privkey)
 
 if __name__ == "__main__":
     unittest.main()
